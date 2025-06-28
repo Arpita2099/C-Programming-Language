839445cfb52247815c1935752cdafc5203f70962
Arpita Sah <arpitasah750@gmail.com>
2025-06-28 10:10:34 +0545

    Create Operators.cpp
    By using the different types of operators I wrote the C++ program in this.

---
+++ Operators/Operators.cpp
@@ -0,0 +1,56 @@
+//WAP to find the sum of two number.
+#include<iostream>
+Using namespace std;
+int main(){
+  int a, b, sum;
+  cout>>"Enetr the first number: ";
+  cin>>a;
+  cout<<"Enter the Second number: ";
+  cin>>b;
+  sum=a+b;
+  cout<<"Sum of two number "<<a<<"+"<<b <<" is: "<<sum;
+  return 0;
+}
+
+//WAP to find the substract of two number.
+#include<iostream>
+Using namespace std;
+int main(){
+  int a, b, subs;
+  cout>>"Enetr the first number: ";
+  cin>>a;
+  cout<<"Enter the Second number: ";
+  cin>>b;
+  subs=a-b;
+  cout<<"Substraction of two number "<<a<<"+"<<b <<" is: "<<subs;
+  return 0;
+}
+
+//WAP to find the product of two number.
+#include<iostream>
+Using namespace std;
+int main(){
+  int a, b, pro;
+  cout>>"Enetr the first number: ";
+  cin>>a;
+  cout<<"Enter the Second number: ";
+  cin>>b;
+  pro=a*b;
+    cout<<"Product of two number "<<a<<"+"<<b <<" is: "<<pro;
+  return 0;
+}
+
+//WAP to find the division of two number.
+#include<iostream>
+Using namespace std;
+int main(){
+  float a, b, div;
+  cout>>"Enetr the first number: ";
+  cin>>a;
+  cout<<"Enter the Second number: ";
+  cin>>b;
+  div=a/b;
+  cout<<"Division of two number "<<a<<"+"<<b <<" is: "<<div;
+  return 0;
+}
+
