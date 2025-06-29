b3a9aae5635395b89827b18567564ed2dfa0b59f
Arpita Sah <arpitasah750@gmail.com>
2025-06-29 11:57:43 +0545

    Create ternary.cpp

---
+++ ternary.cpp
@@ -0,0 +1,12 @@
+/* Ternary Statement 
+conditio? str1:str2;
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+  int n=23;
+  cout<<(n>=0?"This is a positive number.":"This is a negative number.")<<endl;
+  return 0;
+}
+
